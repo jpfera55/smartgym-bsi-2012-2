fdb6156d67d3ccff73d8c77924b225f7df5513f0
dolalima@gmail.com <dolalima@gmail.com@b3d6a002-ebc7-8cc9-b2c9-8eeba49bda33>
2013-02-24 20:46:26 +0000

    Adicionada classes Paymente

---
+++ trunk/SmartGym/src/smartgym/models/entities/Payment.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartgym.models.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Carlos
+ */
+@Entity
+@Table(name = "payment")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Payment.findAll", query = "SELECT p FROM Payment p"),
+    @NamedQuery(name = "Payment.findById", query = "SELECT p FROM Payment p WHERE p.id = :id"),
+    @NamedQuery(name = "Payment.findByPaymentDate", query = "SELECT p FROM Payment p WHERE p.paymentDate = :paymentDate"),
+    @NamedQuery(name = "Payment.findByPaymentValue", query = "SELECT p FROM Payment p WHERE p.paymentValue = :paymentValue")})
+public class Payment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "paymentDate")
+    @Temporal(TemporalType.DATE)
+    private Date paymentDate;
+    @Basic(optional = false)
+    @Column(name = "paymentValue")
+    private double paymentValue;
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Employee employeeId;
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Client clientId;
+
+    public Payment() {
+    }
+
+    public Payment(Integer id) {
+        this.id = id;
+    }
+
+    public Payment(Integer id, Date paymentDate, int paymentValue) {
+        this.id = id;
+        this.paymentDate = paymentDate;
+        this.paymentValue = paymentValue;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public double getPaymentValue() {
+        return paymentValue;
+    }
+
+    public void setPaymentValue(double paymentValue) {
+        this.paymentValue = paymentValue;
+    }
+
+    public Employee getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Employee employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Client getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Client clientId) {
+        this.clientId = clientId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Payment)) {
+            return false;
+        }
+        Payment other = (Payment) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "smartgym.models.entities.Payment[ id=" + id + " ]";
+    }
+    
+}
